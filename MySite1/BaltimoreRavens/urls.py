db68e6d46ffa5b87fc64d3aa5f34add98621b5aa
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-02-09 09:16:38 -0800

    Pages added from dictionary without seperate files

--- MySite1/BaltimoreRavens/urls.py
+++ MySite1/BaltimoreRavens/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("<int:player>", views.player_by_number),
+    path("<str:player>", views.player_text)
 ]
--- MySite1/BaltimoreRavens/views.py
+++ MySite1/BaltimoreRavens/views.py
@@ -14,3 +14,20 @@ def index(request):
 
     return render(request, "BaltimoreRavens/index.html", {
         "roster_key":roster})
+
+def player_by_number(request, player):
+    roster = list(player_roster.keys())
+    if player > len(roster) or player < 1:
+        return HttpResponseNotFound("You entered an invalid numeric player's number")
+    next_player = roster[player-1]
+    return HttpResponseRedirect("/BaltimoreRavens/" + next_player)
+
+def player_text(request, player):
+    try:
+        strToReturn = player_roster[player]
+        return render(request, 'BaltimoreRavens/player_account.html', {
+            "text":strToReturn,
+            "player_name": player.capitalize()
+        })
+    except:
+        return HttpResponseNotFound("Player not valid!")
