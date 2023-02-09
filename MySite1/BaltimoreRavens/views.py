ac2395f5f4bc13d7600b888384488cafe3be4970
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-02-09 09:15:38 -0800

    Dictionary and index.html integrated

--- MySite1/BaltimoreRavens/views.py
+++ MySite1/BaltimoreRavens/views.py
@@ -3,3 +3,14 @@ from django.http import  HttpResponse, Http404, HttpResponseNotFound, HttpRespon
 
 def index(request):
     return render(request, "BaltimoreRavens/index.html")
+
+player_roster = {
+    "Lamar Jackson":["Text1", "Text2", "Text3"],
+    "Lamar Jackson 2":["Text1", "Text2", "Text3"]
+}
+
+def index(request):
+    roster = list(player_roster.keys())
+
+    return render(request, "BaltimoreRavens/index.html", {
+        "roster_key":roster})
