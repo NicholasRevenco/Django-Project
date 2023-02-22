eb8440ab4c2b6d685d5e7d69275d659fdd253932
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-02-22 11:27:27 -0800

    Add information to dictionary

--- MySite1/BaltimoreRavens/views.py
+++ MySite1/BaltimoreRavens/views.py
@@ -5,9 +5,9 @@ def index(request):
     return render(request, "BaltimoreRavens/index.html")
 
 player_roster = {
-    "Lamar Jackson 1":{'name': 'Lamar Jackson 1', 'title': 'linebacker', 'number': '1'},
-    "Lamar Jackson 2":{'name': 'Lamar Jackson 2', 'title': 'kicker', 'number': '2'},
-    "Lamar Jackson 3":{'name': 'Lamar Jackson 3', 'title': 'Quarterback', 'number': '5'}
+    "Lamar Jackson 1":{'name': 'Lamar Jackson 1', 'title': 'linebacker', 'number': '1', 'image': 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3916387.png&w=350&h=254'},
+    "Lamar Jackson 2":{'name': 'Lamar Jackson 2', 'title': 'kicker', 'number': '2', 'image': 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3916387.png&w=350&h=254'},
+    "Lamar Jackson 3":{'name': 'Lamar Jackson 3', 'title': 'Quarterback', 'number': '5', 'image': 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3916387.png&w=350&h=254'}
 }
 
 def index(request):
