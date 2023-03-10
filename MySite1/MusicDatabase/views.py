adaa3cd5767139024ce5eec7d6973723f3cd8341
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-03-10 11:39:31 -0800

    Database followthrough step 4: user hits pages

--- MySite1/MusicDatabase/views.py
+++ MySite1/MusicDatabase/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.db import connection, connections
 
 # Create your views here.
+
+def index(request):
+    sqlQuery = "Select artist.id, artist.Name, artist.Formed from artist order by Name asc"
+    with connection.cursor() as cursor:
+        cursor.execute(sqlQuery)
+        rows = list(cursor.fetchall())
+        connection.close()
+
+    return render(request, "MusicDatabase/index.html", {
+    "artists": rows
+    })
